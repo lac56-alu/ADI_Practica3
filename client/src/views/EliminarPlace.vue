1f75424da9de7f390d13542060cdfeea8fa2fb03
rmg151 <rmg151@alu.ua.es>
2021-12-01 12:29:59 +0100

    Eliminar lugar pero boton SI no redirige bien

--- client/src/views/EliminarPlace.vue
+++ client/src/views/EliminarPlace.vue
@@ -9,7 +9,7 @@
         
         <div class="row">
           <br>
-          <button id="delete_yes">  SÍ </button>
+          <button id="delete_yes" v-on:click="deletePlace">  SÍ </button>
           <router-link to="/myplaces" id = "delete_no" tag="button"> NO </router-link>
         </div>
 
@@ -35,14 +35,31 @@ export default {
   
   methods:{
 
+      async deletePlace(){
+      try{
+        console.log(localStorage)
+        var tokenBearer = 'Bearer ' + localStorage.token;
+        var idPlace = this.$route.params.id
+        const response = await fetch('http://localhost:3000/api/place/' + idPlace, {
+          method: 'DELETE',
+          headers: { 'Authorization': tokenBearer },
+        });
 
-
-
+        var aux = await response.json();
+        console.log(aux);
+        this.detalles = aux['respuesta']
+        console.log(this.myplaces)
+        this.$router.push({path: 'myplaces'}); ////////////////////////////// redirige mal 
+      }catch (error)  {
+        //En ese caso, no mostrar la vista!!!!!!!!!!!
+        console.error(error);
+      }
+    },
 
   },
 
   mounted() {
-      //this.getDetalles()
+      this.deletePlace()
 
   }
 }
