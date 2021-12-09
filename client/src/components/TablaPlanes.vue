37f13d6f57af22d6b523d47f06e40e1a33ff63a3
rmg151 <rmg151@alu.ua.es>
2021-12-09 23:25:06 +0100

    Ver detalles

--- client/src/components/TablaPlanes.vue
+++ client/src/components/TablaPlanes.vue
@@ -7,15 +7,24 @@
       <thead>
         <tr>
           <th>Nombre</th>
+          <th> </th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="p in plans" :key="p.id">
           <td>{{ p.name }}</td>
+          <td > <button v-on:click="mostrarDetalles(p.id)"> Ver detalles </button> </td> 
 
         </tr>
       </tbody>
-    </table>
+    </table> <br>
+
+    <div v-if="this.visiblePlan">     
+        <table id="table" class="table table-striped table-bordered table-hover">
+          <tabla-detalles-planes :detallesPlanes="detallesPlanes"/>
+        </table> <br>
+    </div>
+
   </div>
 </template>
 
@@ -47,11 +56,53 @@ table thead {
 </style>
 
 <script>
+import authservice from '../services/auth-header';
+
+import TablaDetallesPlanes from '@/components/TablaDetallesPlanes.vue';
   export default {
     name: 'tabla-plans',
     props: {
        plans: Array,
     },
+    data(){
+        return{
+          visiblePlan: false,
+          detallesPlanes: []
+        }
+  },
+    components: {
+    TablaDetallesPlanes
+  },
+    methods: {
+      async mostrarDetalles(id){
+      try{
+        console.log("VISIBLE:")
+        console.log(this.visiblePlan)
+        this.visiblePlan = true;
+        console.log(localStorage)
+        //var tokenBearer = 'Bearer ' + localStorage.token;
+        console.log("AQUIIII:")
+        console.log(id)
+        var idPlan = id
+        const response = await fetch('http://localhost:3000/api/plan/' + idPlan, {
+          method: 'GET',
+          headers: { 'Authorization': authservice().Authorization },
+        });
+
+        var aux = await response.json();
+        console.log(aux);
+        this.detallesPlanes = aux['respuesta']
+        
+        console.log(this.visiblePlan)
+      }catch (error)  {
+        //En ese caso, no mostrar la vista!!!!!!!!!!!
+        console.error(error);
+      }
+    }
+    },
+    mounted() {
+      this.mostrarDetalles();
+    }
   }
 </script>
 
