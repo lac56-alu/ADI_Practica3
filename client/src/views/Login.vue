b75ed6d1cc5db11f235056f64d934a2e6ed5bb4c
rmg151 <rmg151@alu.ua.es>
2021-12-10 10:56:44 +0100

    Añadida paginacion listatodos los lugares

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -32,10 +32,9 @@
                                     
                             </div>
                         </form>
-
-                        <div class="form-group">
-                            <div v-if="mensaje" class="alert alert-danger" role="alert">{{mensaje}}</div>
-                        </div>
+                            <div class="form-group">
+                                <div v-if="mensaje" class="alert alert-danger" role="alert">{{mensaje}}</div>
+                            </div>
                     </div>
                 </div>
             </div>
@@ -44,6 +43,7 @@
 </template>
 
 <script>
+
     import { login } from '../services/UserService';
     import User from '../models/user';
 
@@ -108,6 +108,36 @@
 </script>
 
 <style scoped>
+
+.shake {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%,
+  80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%,
+  60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
 
 
 #login .container #login-row #login-column #login-box {
