cba26ea046334a66ec96513f85dacf4750297bb4
rmg151 <rmg151@alu.ua.es>
2021-12-11 18:23:56 +0100

    Tabla lugares loggeado

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import VueRouter from 'vue-router'
 import JwPagination from 'jw-vue-pagination';
+import 'bootstrap'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
